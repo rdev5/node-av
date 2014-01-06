6f86877a7370aa3d5cfdb4211c52da07d5777e3b
Matt Borja <matt.borja@gmail.com>
2014-01-05 22:48:57 -0700

    Added support for multiple reference lookup and ranges

--- ref.js
+++ ref.js
@@ -7,21 +7,38 @@ var keywords = process.argv;
 keywords.shift(); // node
 keywords.shift(); // av-search.js
 
-var book = keywords[0];
-var ref = keywords[1].split(':');
-if (book.match(/\d/)) {
-   book += ' ' + keywords[1];
-   var ref = keywords[2].split(':');
+// v2.0
+var keywords = keywords.join(' ');
+var ref_list = keywords.split(';');
+
+var ref_list_where = [];
+for (var i = 0; i < ref_list.length; i++) {
+   var keywords = ref_list[i].trim().split(' ');
+
+   var book = keywords[0];
+   var ref = keywords[1].split(':');
+   if (book.match(/\d/)) {
+      book += ' ' + keywords[1];
+      var ref = keywords[2].split(':');
+   }
+   var chapter = ref[0];
+   var verse = ref[1];
+
+   var where = [];
+   where.push("book.name LIKE '" + book + "%'");
+   where.push("bible.chapter = '" + chapter + "'");
+
+   if (verse.match(/^\d+$/)) {
+      where.push("bible.verse = '" + verse + "'");
+   } else if(verse.match(/^(\d+)\-(\d+)$/)) {
+      var range = verse.match(/^(\d+)\-(\d+)$/);
+      where.push("(bible.verse >= '" + range[1] + "' AND bible.verse <= '" + range[2] + "')");
+   }
+
+   ref_list_where.push('(' + where.join(' AND ') + ')');
 }
-var chapter = ref[0];
-var verse = ref[1];
 
-var where = [];
-where.push("book.name LIKE '" + book + "%'");
-where.push("bible.chapter = '" + chapter + "'");
-where.push("bible.verse = '" + verse + "'");
-
-var q = "SELECT book.name, bible.chapter, bible.verse, bible.text, book.name || ' ' || bible.chapter || ':' || bible.verse AS ref FROM bible INNER JOIN book ON book.id = bible.book_id WHERE " + where.join(' AND ');
+var q = "SELECT book.name, bible.chapter, bible.verse, bible.text, book.name || ' ' || bible.chapter || ':' || bible.verse AS ref FROM bible INNER JOIN book ON book.id = bible.book_id WHERE " + ref_list_where.join(' OR ');
 
 db.all(q, function (err, results) {
    if (err) {
